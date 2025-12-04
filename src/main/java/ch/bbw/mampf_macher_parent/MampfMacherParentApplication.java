314dea95aac38994673fdd9f6d47e854143e7172
Hagmann Stephan <stephanhagmann@outlook.com>
2025-12-04 10:34:06 +0000

    first commit

---
+++ src/main/java/ch/bbw/mampf_macher_parent/MampfMacherParentApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.mampf_macher_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MampfMacherParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MampfMacherParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/bbw/mampf_macher_parent/MampfMacherParentApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.mampf_macher_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MampfMacherParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
