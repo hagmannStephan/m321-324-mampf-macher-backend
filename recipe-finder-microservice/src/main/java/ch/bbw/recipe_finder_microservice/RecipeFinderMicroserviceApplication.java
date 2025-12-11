631f7eb0bdc50ecf5cd1c74ae09181fb576a2fba
Hagmann Stephan <stephanhagmann@outlook.com>
2025-12-11 07:37:50 +0000

    chore: added first micro service

---
+++ recipe-finder-microservice/src/main/java/ch/bbw/recipe_finder_microservice/RecipeFinderMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.recipe_finder_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeFinderMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeFinderMicroserviceApplication.class, args);
+	}
+
+}
---
+++ recipe-finder-microservice/src/test/java/ch/bbw/recipe_finder_microservice/RecipeFinderMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.recipe_finder_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeFinderMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
