cb1ee5463319a202c36255c1cf81b773b28cd1b3
Hagmann Stephan <stephanhagmann@outlook.com>
2025-12-18 07:42:11 +0000

    chore: created connection to the db

--- recipe-finder-microservice/src/main/java/ch/bbw/recipe_finder_microservice/delegate/RecipeFinderDelegate.java
+++ recipe-finder-microservice/src/main/java/ch/bbw/recipe_finder_microservice/delegate/RecipeFinderDelegate.java
@@ -1,13 +1,30 @@
 package ch.bbw.recipe_finder_microservice.delegate;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.bbw.recipe_finder_microservice.entity.Ingredient;
+import ch.bbw.recipe_finder_microservice.repository.IngredientRepository;
 
 @RestController
 public class RecipeFinderDelegate {
 
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello from recipe finder delegate!";
+    }
+
+    @GetMapping("/ingredients")
+    public List<String> getIngredients() {
+        return ingredientRepository.findAll().stream()
+                .map(Ingredient::getName)
+                .collect(Collectors.toList());
     }
 }
---
+++ recipe-finder-microservice/src/main/resources/sql/tables.sql
@@ -0,0 +1,35 @@
+-- Active: 1766042947393@@postgresdb@5432@postgres@public
+-- 1. Create tables
+CREATE TABLE preferences (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL
+);
+
+CREATE TABLE recipe (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    description VARCHAR(128),
+    recipe_url VARCHAR(256)
+);
+
+CREATE TABLE ingredients (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(64) NOT NULL
+);
+
+-- 2. Create junction tables (many-to-many)
+CREATE TABLE preferences_recipe (
+    preferences_id BIGINT NOT NULL,
+    recipe_id BIGINT NOT NULL,
+    PRIMARY KEY (preferences_id, recipe_id),
+    FOREIGN KEY (preferences_id) REFERENCES preferences(id) ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) REFERENCES recipe(id) ON DELETE CASCADE
+);
+
+CREATE TABLE ingredients_recipe (
+    ingredients_id BIGINT NOT NULL,
+    recipe_id BIGINT NOT NULL,
+    PRIMARY KEY (ingredients_id, recipe_id),
+    FOREIGN KEY (ingredients_id) REFERENCES ingredients(id) ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) REFERENCES recipe(id) ON DELETE CASCADE
+);
