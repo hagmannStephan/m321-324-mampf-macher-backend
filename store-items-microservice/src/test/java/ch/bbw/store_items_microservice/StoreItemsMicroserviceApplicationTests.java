c48f9c4b2e97829e437a25c1f0c647815490b893
esilva <esilva@ethz.ch>
2026-01-08 15:42:59 +0000

    made active profile dev

--- store-items-microservice/src/test/java/ch/bbw/store_items_microservice/StoreItemsMicroserviceApplicationTests.java
+++ store-items-microservice/src/test/java/ch/bbw/store_items_microservice/StoreItemsMicroserviceApplicationTests.java
@@ -2,8 +2,10 @@ package ch.bbw.store_items_microservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class StoreItemsMicroserviceApplicationTests {
 
 	@Test
